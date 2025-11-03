ce54aa28baa093a7212c49d96e87802a4cd5b9db
Lucas Franciscon <194494292+Lord-of-Souls@users.noreply.github.com>
2025-11-03 13:56:27 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # vehicles
+## este projeto visa criar um aplicativo web que exibe dados e tabelas sobre a relação de preço e quilometragem de carros nos Estados Unidos
---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')  # lendo os dados
+hist_button = st.button('Criar histograma')  # criar um botão
+scatter_button = st.checkbox("criar gráfico de dispersão") # checkbox para gráfico de dispersão
+
+if hist_button:  # se o botão for clicado
+    # escrever uma mensagem
+    if scatter_button: # se o checkbox estiver ativo
+        st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+        # criar um histograma
+        fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
+
+        # exibir um gráfico Plotly interativo
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+
+        st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+
+        # criar um histograma
+        fig = px.histogram(car_data, x="odometer")
+
+        # exibir um gráfico Plotly interativo
+        st.plotly_chart(fig, use_container_width=True)
+
